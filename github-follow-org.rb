26e1cd81297db7db2c88d536fda52795ef2f2d17
Braintree <code@getbraintree.com>
2013-08-30 11:38:26 -0500

    Renamed file and allow entering org name from command line

--- github-follow-org.rb
+++ github-follow-org.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'bundler/setup'
+
 require 'rest-client'
 require 'io/console'
 require 'json'
@@ -8,6 +11,9 @@ username = gets.chomp!
 puts "Password: "
 password = STDIN.noecho(&:gets).chomp!
 
+puts "Org to follow: "
+org = gets.chomp!
+
 GITHUB_URL = "https://#{username}:#{password}@api.github.com"
 
 # get followers
@@ -15,7 +21,7 @@ r = RestClient.get GITHUB_URL + "/user/following"
 followers = JSON.parse(r.to_s).map {|u| u["login"]}
 
 # get BT members
-r = RestClient.get GITHUB_URL + "/orgs/braintree/members"
+r = RestClient.get GITHUB_URL + "/orgs/#{org}/members"
 members = JSON.parse(r.to_s).map {|u| u["login"]}
 
 # follow members
